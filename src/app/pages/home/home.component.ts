5a54f813240b4e54be9d8f156114026c7ed90090
annicole <annicole_rosa@outlook.com>
2019-08-12 14:37:38 -0500

    added img on home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {CiaService} from '../../services/cia.service';
+import Swal from 'sweetalert2';
+import {Cia} from '../../models/cia';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  cia:Cia = new Cia();
+  urlImg:string;
+  constructor(private router:Router,private ciaService:CiaService) { }
 
   ngOnInit() {
+    this.urlImg = "img/ICMA_AUTOMATION-01.png"
   }
 
+  async getCia(){
+    try{
+      let resp = await this.ciaService.readCia(1).toPromise();
+      if (resp.code == 200) {
+        this.cia = resp.cia;
+        console.log(resp);
+      }
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+  navigateTo(url:String){
+    url = '/'+url;
+    this.router.navigate([url]);
+  }
 }
