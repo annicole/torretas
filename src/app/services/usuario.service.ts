e7eb82cb530f3f8b89733e942aad7706e93faf50
annicole <annicole_rosa@outlook.com>
2019-10-10 15:45:21 -0500

    added token headers

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -12,30 +12,31 @@ export class UsuarioService {
   private httpHeaders = new HttpHeaders({ 'Content-type': 'application/json' })
   constructor(private http: HttpClient) { }
 
-  getUsuarios(name:string,depatamento:string): Observable<any> {
+  getUsuarios(name:string,depatamento:string,token): Observable<any> {
     let params = new HttpParams();
     params = params.append('busqueda',name);
     params = params.append('departamento',depatamento);
+    this.httpHeaders.set("Authorization",token);
     return this.http.get(this.url + '/usuarios',{headers:this.httpHeaders,params:params});
   }
 
-  create(usuario: Usuario): Observable<any> {
+  create(usuario: Usuario,token): Observable<any> {
+    this.httpHeaders.set("Authorization",token);
     return this.http.post<any>(this.url + '/usuarios', usuario, { headers: this.httpHeaders });
   }
 
-  readUsuario(id: number): Observable<any> {
-    return this.http.get(`${this.url + '/read'}/${id}`);
+  readUsuario(id: number,token): Observable<any> {
+    this.httpHeaders.set("Authorization",token);
+    return this.http.get(`${this.url + '/read'}/${id}`,{headers:this.httpHeaders});
   }
 
-  read(id: number): Observable<any> {
-    return this.http.get(`${this.url + '/read'}/${id}`);
+  delete(id: number,token): Observable<any> {
+    this.httpHeaders.set("Authorization",token);
+    return this.http.delete<any>(`${this.url + '/read'}/${id}`,{headers:this.httpHeaders});
   }
 
-  delete(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.url + '/read'}/${id}`);
-  }
-
-  update(usuario: Usuario) {
+  update(usuario: Usuario,token) {
+    this.httpHeaders.set("Authorization",token);
     return this.http.put(`${this.url + '/read'}/${usuario.id}`, usuario, { headers: this.httpHeaders });
   }
 }
