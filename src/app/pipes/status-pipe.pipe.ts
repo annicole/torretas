b5009f168ca9e79eebb5c61334adfed13dfc7d43
NICOLE\Nicole <annicole_rosas@outlook.com>
2021-02-03 15:38:13 -0600

    changed editar-status

--- src/app/pipes/status-pipe.pipe.ts
+++ src/app/pipes/status-pipe.pipe.ts
@@ -8,8 +8,8 @@ export class StatusPipePipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
     if(args!=null){
       switch(value){
-        case 0: return "Pendiente";
-        case 1: return "En proceso";
+        case 0: return "En proceso";
+        case 1: return "Pendiente";
         case 2: return "En suspensión";
         case 3: return "Terminado";
       }
